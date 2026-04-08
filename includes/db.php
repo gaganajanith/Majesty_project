af8703119a249cb2cc039c465cab192d4deb276e
hasithaea <hasitha2k4@gmail.com>
2026-04-08 18:17:31 +0530

    feat: Finalized hybrid environment support (docker & Localhost)

--- includes/db.php
+++ includes/db.php
@@ -1,8 +1,16 @@
 <?php
-$conn = mysqli_connect('localhost','root','','testshopdb');
+
+//check whether we are running inside a docker container
+
+$host = (getenv('DOCKER_ENV'))?'db':'localhost';
+$user =  'root';
+$pass = (getenv('DOCKER_ENV'))?'root':'';
+$dbname = 'testshopdb';
+
+$conn = mysqli_connect($host,$user,$pass,$dbname);
 
 if(!$conn)
     {
-        die("database connection faild");
+        die("Database connection faild". mysqli_connect_error());
     }
 ?>
