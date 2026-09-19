b68c980cb7dea29cbc18a1ca93df936c06b0cc5d
gagana_janith <gaganajanith585@gmail.com>
2026-09-19 22:10:19 +0530

    add a banner to top

--- pages/cart/cart.php
+++ pages/cart/cart.php
@@ -4,106 +4,118 @@ session_start();
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Majesty | Cart</title>
-        
+
     <link rel="stylesheet" href="/Majesty_project/pages/cart/cart.css">
 </head>
 
 <body>
 
- <!--navigation bar-->
+    <!--navigation bar-->
     <header class="navbar">
-        <a href="/Majesty_project/pages/home/home.html"><img src="/Majesty_project/assets/images/majesty(1).png" alt="logo" width="120" height="40"></a>
+        <a href="/Majesty_project/pages/home/home.html"><img src="/Majesty_project/assets/images/majesty(1).png"
+                alt="logo" width="120" height="40"></a>
         <nav class="nav_bar">
             <!-- search bar -->
-                <div class="search-container">
-                    <input type="text" placeholder="Search...">
-                    <button><img src="/Majesty_project/assets/images/search_bar.png" alt="search" width="15px" height="15px"></button>
-                </div>
+            <div class="search-container">
+                <input type="text" placeholder="Search...">
+                <button><img src="/Majesty_project/assets/images/search_bar.png" alt="search" width="15px"
+                        height="15px"></button>
+            </div>
             <ul class="nav-links">
-                
+
                 <!--nav links-->
                 <li><a href="/Majesty_project/pages/shop/mens/mens.html">MENS</a></li>
                 <li><a href="/Majesty_project/pages/shop/womens/womens.html">WOMENS</a></li>
                 <li><a href="/Majesty_project/pages/auth/sign_up.html">SIGNUP</a></li>
-                <li><a href="/Majesty_project/pages/cart/cart.php"><img src="/Majesty_project/assets/images/Addw.png" class="cart"></a></li>
+                <li><a href="/Majesty_project/pages/cart/cart.php"><img src="/Majesty_project/assets/images/Addw.png"
+                            class="cart"></a></li>
             </ul>
         </nav>
     </header>
 
+    <!--top banner-->
+    <section class="cart-hero">
+        <div class="cart-hero-overlay">
+            <h1>Your Cart</h1>
+            <p><b>Good choices. Now make them yours.</b></p>
+        </div>
+    </section>
+
     <div class="page_layout">
-     
-    <div class="cart_sidebar">
-        <ul>
-            <li><a href="/Majesty_project/pages/home/home.html#shop_now"> Add more items</a> </li>
-            <li><a href="/Majesty_project/logic/logout.logic.php">Logout </a> </li>
-            <li><a href="/Majesty_project/logic/reset_cart.logic.php">Reset Cart</a></li>
-            <li class="checkout_btn"><a href="/Majesty_project/pages/auth/login.html">CHECK OUT</a></li>
-        </ul>
+
+        <div class="cart_sidebar">
+            <ul>
+                <li><a href="/Majesty_project/pages/home/home.html#shop_now"> Add more items</a> </li>
+                <li><a href="/Majesty_project/logic/logout.logic.php">Logout </a> </li>
+                <li><a href="/Majesty_project/logic/reset_cart.logic.php">Reset Cart</a></li>
+                <li class="checkout_btn"><a href="/Majesty_project/pages/auth/login.html">CHECK OUT</a></li>
+            </ul>
+        </div>
+
+        <div class="cart_main">
+
+
+            <?php
+            if (empty($_SESSION['cart'])):
+                ?>
+                <center>
+                    <h1>Cart is empty !!!</h1>
+                    <br><br><br><br><br>
+                    <a href="/Majesty_project/pages/home/home.html#shop_now"><button class="shop-btn">Shop Now</button></a>
+                </center>
+            <?php
+            else:
+                ?>
+
+
+                <table border="1" cellpadding="10">
+
+                    <tr>
+                        <th>Item</th>
+                        <th>Price</th>
+                        <th>Qty</th>
+                        <th>Total</th>
+                    </tr>
+
+                    <?php
+                    $grand = 0;
+                    foreach ($_SESSION['cart'] as $item):
+                        $total = $item['price'] * $item['qty'];
+                        $grand += $total;
+                        ?>
+
+                        <tr>
+                            <td><?= $item['name'] ?></td>
+                            <td align="right">LKR <?= $item['price'] ?></td>
+                            <td align="center"><?= $item['qty'] ?></td>
+                            <td align="right">LKR <?= $total ?></td>
+                        </tr>
+
+                    <?php endforeach; ?>
+
+                    <tr>
+                        <td colspan="3" align="center"><b>Grand Total</b></td>
+                        <td align="right"><b>LKR <?= $grand ?></b></td>
+                    </tr>
+
+                </table>
+
+            <?php endif; ?>
+
+        </div>
+
     </div>
 
-<div class="cart_main">
 
 
-<?php
-    if(empty($_SESSION['cart'])):
-?>
-    <center>
-    <h1>Cart is empty !!!</h1>
-    <br><br><br><br><br>
-    <a href="/Majesty_project/pages/home/home.html#shop_now"><button class="shop-btn">Shop Now</button></a>
-    </center>
-<?php 
-    else: 
-?>
+    </div>
 
-
-<table border="1" cellpadding="10">
-
-    <tr>
-        <th>Item</th>
-        <th>Price</th>
-        <th>Qty</th>
-        <th>Total</th>
-    </tr>
-
-    <?php 
-        $grand = 0;
-        foreach ($_SESSION['cart'] as $item):
-        $total = $item['price']*$item['qty'];
-        $grand += $total;
-    ?>
-
-    <tr>
-        <td><?= $item['name'] ?></td>
-        <td align="right">LKR  <?= $item['price'] ?></td>
-        <td align="center"><?= $item['qty'] ?></td>
-        <td align="right">LKR  <?= $total ?></td>
-    </tr>
-
-    <?php endforeach; ?>
-
-    <tr>
-        <td colspan="3" align="center"><b>Grand Total</b></td>
-        <td align="right"><b>LKR  <?= $grand ?></b></td>
-    </tr>
-
-</table>
-
-<?php endif; ?>
-
-</div>
-
-</div>
-
-
-    
-</div>
-
-  <!-- Footer -->
+    <!-- Footer -->
 
     <footer class="footer">
         <div class="footer-column">
@@ -127,6 +139,7 @@ session_start();
             <br>
             <pre>&copy; 2026 | Majesty | All Rights Reserved |</pre>
         </div>
-    </footer>   
+    </footer>
 </body>
+
 </html>
